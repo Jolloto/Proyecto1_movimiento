eab77ada603d1d1b490c75d1b1922e89ce32c927
Jolloto <jolloto02@gmail.com>
2023-10-23 10:57:33 +0200

    Proyecto1_movimiento

---
+++ Assets/Scripts/Follow_Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Player : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset = new Vector3(x: 0, y: 5, z: -10);
+
+
+    private void Update() 
+    {
+        transform.position = player.transform.position + offset;
+
+    
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float lateralspeed;
+
+    [SerializeField] private Vector3 offset = new Vector3(x:0, y:5, z:-10);
+    
+
+    private float horizontalInput;
+    private float verticalInput;
+
+       void Update()
+    {
+        //La detección de inputs debe ir en el Update
+        horizontalInput = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalInput);
+
+
+        // Recomendación si trabajamos con direcciones, que sean vector
+        // Vector3.right se corresponde con (1, 0, 0)
+        // Vector3.left se corresponde con (-1, 0, 0)
+        // Vector3.up se corresponde con (0, 1, 0)
+        // Vector3.down se corresponde con (0, -1, 0)
+        // Vector3.forward se corresponde con (0, 0, 1)
+        // Vector3.back se corresponde con (0, 0, -1)
+
+        // Movimiento en función del sistema de coordenadas local (el del objeto)
+        // Movimiento hacia adelante automático
+        //transform.Translate(translation:Vector3.forward * speed * Time.deltaTime);
+
+        // Movimiento hacia adelante afectado por el input del usuario (vertical)
+        transform.Translate(translation:Vector3.forward * speed * Time.deltaTime);
+
+        // Movimiento lateral afectado por el input
+        transform.Translate(translation:Vector3.right * lateralspeed * Time.deltaTime * horizontalInput);
+    }
+}
